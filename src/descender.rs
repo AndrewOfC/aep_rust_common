d25811cc8d23f02fc943b8580ea5e96ad70e52ff
ANDREW PAGE <andrew.e.page@gmail.com>
2025-07-12 19:46:15 -0400

    Added function to find configuration files
    dynamic Descender
    
    missing methods
    
    descent fixes
    
    Absorbtion of functionality from ucompleter
    migration to Descender trait
    
    additional tests from ucompleter

---
+++ src/descender.rs
@@ -0,0 +1,14 @@
+use std::io::Write;
+
+pub trait Descender<W: Write + ?Sized>: Send + Sync {
+
+    fn set_root(&mut self, root: &str);
+
+    fn get_string_field_or_parent(&self, path: &str, field: &str) -> Result<String, String>;
+    fn get_int_field_or_parent(&self, path: &str, field: &str) -> Result<i64, String>;
+    fn get_bool_field_or_parent(&self, path: &str, field: &str) -> Result<bool, String>;
+    fn get_float_field_or_parent(&self, path: &str, field: &str) -> Result<f64, String>;
+
+    fn write_completions(&self, writer: &mut dyn Write, ipath: &str, add_descriptions: bool, zsh_mode: bool) -> std::io::Result<()> ;
+}
+
